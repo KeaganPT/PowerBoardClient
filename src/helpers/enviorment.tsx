97093b7b8b9bc244d7027f2799c8a5202ac101d7
Keagan Talbot <keaganpt@gmail.com>
2021-01-13 10:38:37 -0500

    heroku client is not connecting to either server getting errors that don't make sense such as no access for enpoints with no validation required

--- src/helpers/enviorment.tsx
+++ src/helpers/enviorment.tsx
@@ -4,6 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
 
         APIURL = 'http://localhost:3000';
+        // APIURL = 'https://kpt-powerboard.herokuapp.com';
         break;
     case 'kpt-power-board-client.herokuapp.com/':
         APIURL = 'https://kpt-powerboard.herokuapp.com'
